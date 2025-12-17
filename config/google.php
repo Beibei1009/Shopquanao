265910c0e0d56fc9b7be20acbae2fd13dd16d450
Trần Thị Như Ý <trany.hg2001@gmail.com>
2025-12-18 01:08:45 +0700

    url callback google

--- config/google.php
+++ config/google.php
@@ -4,5 +4,6 @@
 return [
     'client_id' => '959279991984-ab0gr9f3em4dglvgk7eihssvjd5l25hu.apps.googleusercontent.com',
     'client_secret' => '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',
-    'redirect_uri' => 'http://localhost:8000/auth/google/callback',
+    'redirect_uri' => 'https://shopquanao.fly.dev/auth/google/callback',
+    //  'redirect_uri' => 'http://localhost:8000/auth/google/callback',
 ];
